0611b821b4e39d0bfa769cac949f8e330ac3bb6c
Marion Schleifer <marion.schleifer@simplificator.com>
2017-09-30 15:26:30 +0200

    modern class syntax

--- js/app.js
+++ js/app.js
@@ -1,84 +1,88 @@
 // Enemies our player must avoid
-var Enemy = function (x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-    this.x += this.speed * dt;
-
-    // Make sure that the enemy moves in again from the left, after going off the field on the right
-    if (this.x > 550) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
+class Enemy {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
     }
 
-    // Check if player is hit by an enemy
-    if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200;
-        player.y = 380;
-    }
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        this.x += this.speed * dt;
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        // Make sure that the enemy moves in again from the left, after going off the field on the right
+        if (this.x > 550) {
+            this.x = -100;
+            this.speed = 100 + Math.floor(Math.random() * 512);
+        }
+
+        // Check if player is hit by an enemy
+        if (player.x < this.x + 60 &&
+            player.x + 37 > this.x &&
+            player.y < this.y + 25 &&
+            30 + player.y > this.y) {
+            player.x = 200;
+            player.y = 380;
+        }
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Define a class for player
-var Player = function (x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    this.sprite = 'images/char-boy.png';
-};
-
-// Update the position of the player
-Player.prototype.update = function () {
-    if (this.y > 380) {
-        this.y = 380;
+class Player {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/char-boy.png';
     }
 
-    if (this.x > 400) {
-        this.x = 400;
+    // Update the position of the player
+    update() {
+        if (this.y > 380) {
+            this.y = 380;
+        }
+
+        if (this.x > 400) {
+            this.x = 400;
+        }
+
+        if (this.x < 0) {
+            this.x = 0;
+        }
+
+        if (this.y < 0) {
+            this.x = 200;
+            this.y = 380;
+        }
     }
 
-    if (this.x < 0) {
-        this.x = 0;
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
-    if (this.y < 0) {
-        this.x = 200;
-        this.y = 380;
-    }
-};
-
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Check for user input for the movement of the player
-Player.prototype.handleInput = function (pressedKey) {
-    switch (pressedKey) {
-        case 'left':
-            this.x -= this.speed + 50;
-            break;
-        case 'up':
-            this.y -= this.speed + 30;
-            break;
-        case 'right':
-            this.x += this.speed + 50;
-            break;
-        case 'down':
-            this.y += this.speed + 30;
-            break;
+    // Check for user input for the movement of the player
+    handleInput(pressedKey) {
+        switch (pressedKey) {
+            case 'left':
+                this.x -= this.speed + 50;
+                break;
+            case 'up':
+                this.y -= this.speed + 30;
+                break;
+            case 'right':
+                this.x += this.speed + 50;
+                break;
+            case 'down':
+                this.y += this.speed + 30;
+                break;
+        }
     }
 }
 
